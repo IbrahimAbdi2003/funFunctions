00525ce39cd9cfc4f461445d8e8ae9eea1e5058a
Ibrahim Abdi <ibrahimabdi2367@gmail.com>
2023-02-07 09:03:35 -0500

    homework

---
+++ app.js
@@ -0,0 +1,26 @@
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// function addToArray(array, value) {
+//   array.push(value);
+//   return array;
+// }
+
+// console.log(addToArray(arr, 11));
+
+// let myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// function spliceArray(array) {
+//     array.splice(2, 2, 11, 12);
+//     return array;
+// }
+
+// console.log(spliceArray(myArray));
+
+// function getArraySlice(arr) {
+//     let newArray = arr.slice(2, 5);
+//     return newArray;
+//   }
+
+//   let originalArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let slicedArray = getArraySlice(originalArray);
+// console.log(slicedArray);
